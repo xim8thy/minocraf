498334e5416bcafe6829cc8a77e99da4dd1cab1e
xim8thy <128106786+xim8thy@users.noreply.github.com>
2023-06-29 18:19:30 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ var player_object="";
 var block_image__object="";
 function player_update()
 {
-    fabric.Image.fromURL("descarga (1).jpg " , function(img){
-        player_object= img;
+    fabric.Image.fromURL("descarga (1).jpg " , function(Img){
+        player_object= Img;
         player_object.scaleToWidth(150);
         player_object.scaleToHeight(140);
         player_object.set({
         top:player_y,
-        left:player_y
+        left:player_x
     } ) ;
   canvas.add(player_object);
     }
@@ -23,13 +23,13 @@ function player_update()
 }
 function new_image(get_image)
 {
-    fabric.image.fromURL( get_image , function(img){
-        block_image_object= img;
+    fabric.Image.fromURL( get_image , function(Img){
+        block_image_object= Img;
         block_image_object.scaleToWidth(block_image_width);
         block_image_object.scaleToHeight(block_image_height);
         block_image_object.set({
         top:player_y,
-        left:player_y
+        left:player_x
     } ) ;
   canvas.add(block_image_object);
     }
@@ -88,73 +88,7 @@ function my_keydown(e)
     keyPressed = e.keyCode;
     console.log(keyPressed);
     if (e.shiftKey == true && keyPressed == '80')
-    if (keyPressed == '37')
-    {
-        left();
-        console.log("left");
-    }
-    if (keyPressed == '39')
-    {
-        right();
-        console.log("right");
-    }
-    if (keyPressed == '38')
-    {
-        up();
-        console.log("up");
-    }
-
-    if(keyPressed == '40')
-    {
-        down();
-        console.log("down");
-    }
-    if(keyPressed == '87')
-    {
-        new_image('wall.jpg');
-        console.log("W");
-    }
-    if(keyPressed == '76')
-    {
-        new_image('light_green.png');
-        console.log("l");
-    }
-    if(keyPressed == '71')
-    {
-        new_image('ground.png');
-        console.log('g');
-    }
-    if(keyPressed == '84')
-    {
-        new_image('trunk.jpg');
-        console.log("t");
-    }
-    if(keyPressed == '82')
-    {
-        new_image('roof.jpg');
-        console.log("r");
-    }
-    if(keyPressed == '89')
-    {
-        new_image('yellow_wall.png');
-        console.log("y");
-    }
-    if(keyPressed == '68')
-    {
-        new_image('dark_green.png');
-        console.log("d");
-    }
-    if(keyPressed == '85')
-    {
-        new_image('unique.png');
-        console.log("u");
-    }
-    if(keyPressed == '67')
-    {
-        new_image('cloud.jpg');
-        console.log("c");
-    }
-}
+   
 
 
    {
@@ -239,7 +173,7 @@ function my_keydown(e)
         new_image('cloud.jpg');
         console.log("c");
     }
-
+}
 
 
 
